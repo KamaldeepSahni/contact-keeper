caf6f3a479b03a81ddf8ea90f2a483b888410158
Kamaldeep <ksahni687@gmail.com>
2020-12-20 00:27:18 +0530

    Refactor

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -93,7 +93,7 @@ const AuthState = props => {
     } catch (error) {
       dispatch({
         type: LOGIN_FAIL,
-        payload: error.response.data.msg,
+        payload: error.response.msg,
       });
     }
   };
